aefb96e6353b8a09aa9b6ed853f2993b37d03e1c
Shahzeb Akhtar <shahzebakhtar66@gmail.com>
2024-09-03 23:55:07 +0530

    created signup

---
+++ backend/DB_URL.js
@@ -0,0 +1,3 @@
+const DB_URL = "mongodb+srv://Shahzeb:61aabOIkQbyUwPVR@amazing-blog-db.qishp.mongodb.net/?retryWrites=true&w=majority&appName=amazing-blog-db"
+
+export default DB_URL;
--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,62 @@
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
+import DB_URL from "./DB_URL.js";
+import {z} from "zod"
+import User from "./model/model.js";
+
 
 const app = express();
 
 app.use(cors());
 app.use(express.json())
 
+
+mongoose.connect(DB_URL).then(()=>console.log("MONGODB connected")).catch(()=>console.error("Mongodb connection error"))
+
+
+const userZod = z.object({
+    name: z.string().min(6, "Name is too short").max(10,"Name is too long"), 
+    email: z.string().email("Invalid email address"), 
+    password: z.string().min(6, "Password must be at least 6 characters long")
+    .max(8,"Password is too long")
+});
+
+//*------------------------------------------------------
+
 app.get("/",(req,res)=>{
     return res.json({message:"This is / route"});
 })
 
-app.post("/create-blog",(req,res)=>{
-    let title = "";
-    const {a,b,c,title:d} = req.body;
-    return res.json({a,b,c,d})
+
+
+app.post("/signup",async (req,res)=>{
+    const {name,email,password} = req.body;
+    const result = userZod.safeParse({name,email,password});
+    if (!result.success) {
+        return res.status(400).json({ errors: result.error.errors.map((i)=>
+        {
+            return res.json({message:i.message})
+        }
+        )});
+    }
+    //* just creating a new user
+    const newUser  =  new User({
+        name,
+        email,
+        password
+    })
+    try {
+        newUser.save();
+        console.log("NEW USER CREATED");
+    } catch (error) {
+        console.log("ERROR->",error);
+        
+    }
+
+    return res.json({message:"SUCCESS",username:name,email,password,createdAt:Date.now()});
 })
 
 
-
-
+//* just listening on port 3000
 app.listen(3000,()=>console.log(`Server running on ${3000}`))
